271b3a666631ed42048f7aa539267bc16039aa40
Girma Nigusse <xgirma@gmail.com>
2020-04-08 19:46:08 -0400

    chrome 80 only, until chrome 81 is available in GHActions

--- features/support/steps.js
+++ features/support/steps.js
@@ -1,49 +1,36 @@
-const { Given, When, Then, AfterAll } = require("cucumber");
+const { setDefaultTimeout, Given, When, Then, AfterAll } = require("cucumber");
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
 const wd = require("wd");
+const config = require("../../config");
 
 const url = "https://e2e-boilerplate.github.io/sandbox/";
-
+setDefaultTimeout(50 * 1000);
 chai.use(chaiAsPromised);
 chai.should();
+
 chaiAsPromised.transferPromiseness = wd.transferPromiseness;
 const browser = wd.promiseChainRemote();
 
 Given(/^Navigate to the sandbox$/, () => {
-  return process.env.GITHUB_ACTIONS
-    ? browser
-        .init({
-          browserName: "chrome",
-          "goog:chromeOptions": { args: ["--headless", "--disable-gpu"] },
-        })
-        .get(url)
-    : browser
-        .init({
-          browserName: "chrome",
-        })
-        .get(url);
+  return config(url, browser);
 });
 
 AfterAll(() => {
   browser.quit();
 });
 
-When(/^I am on the sandbox page$/, (done) => {
-  browser
-    .title()
-    .then((title) => {
-      title.should.equal("Sandbox");
-    })
-    .nodeify(done);
+When(/^I am on the sandbox page$/, () => {
+  return browser.title().then((title) => {
+    title.should.equal("Sandbox");
+  });
 });
 
-Then(/^The page header should be "([^"]*)"$/, (header, done) => {
-  browser
+Then(/^The page header should be "([^"]*)"$/, (header) => {
+  return browser
     .elementByTagName("h1")
     .text()
-    .then((text) => {
-      text.should.equal(header);
-    })
-    .nodeify(done);
+    .then((title) => {
+      title.should.equal(header);
+    });
 });
